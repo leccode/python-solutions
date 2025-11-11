fc7aa516250dbcff9e9d48d50fc12c17d72848b2
leccode <costas.lekkos@gmail.com>
2025-11-11 23:26:42 +0200

    feat(console app): display hello world

---
+++ chapter1/hello_world.py
@@ -0,0 +1,18 @@
+'''
+  Author: Constantine Lekkos
+  Created Date: 11/11/2025 (dd/MM/yyyy)
+  Purpose: Coding Factory exercises
+  Stack: Python
+'''
+
+'''
+  This program is the starting point of software engineering!
+  Stores the needed info into a variable and then displays it.
+  So => "Hello World 8-)"
+'''
+
+# A variable that stores the needed string with a Unicode escape sequence
+input_string = "Hello World \U0001f60e"
+
+# Built-in method to display the content of the variable
+print(input_string)
